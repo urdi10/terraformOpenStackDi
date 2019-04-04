a47f813d1f76c9307b12e9de88d2e2565f78c5f9
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2019-04-04 19:22:27 +0200

    creacion de 1 vm y 2 vm nodos

--- ubuntuVM.tf
+++ ubuntuVM.tf
@@ -5,7 +5,7 @@ variable "user_name" {default = "jjcanada"}
 ## 2. Abrir el archivo token.json generado en la operación curl y copiar el token de acceso
 
 variable "my_token" {
-  default = "PASTE_THE_TOKEN_HERE"  
+  default = "gAAAAABcphnpSrof9T7-whVBjDQ-6jJuWQl-XqLnA2O7bCVcneJ1XNJrADGZMNnPq8pYrZ22cYNTN1FgYim56Nz0vHo55Dmo6nZa4IIsvY8VHvqYnE5dWODqTNu00Nma13aMYwo8ZqcGGbwlWZukOGEtMYBXtHN8T0mWGqcU8oWqXE1GyVU4FCs"  
 }
 
 variable "project_name" {
@@ -26,8 +26,9 @@ provider "openstack" {
   region      = "RegionOne"
 }
 
-
-# Crear la Virtual Machine 
+########################################
+# Crear la Virtual Machine
+########################################
 
 resource "openstack_compute_instance_v2" "mi_tf_instance" {
   name      = "ub18vm-cnsa"
@@ -54,4 +55,65 @@ resource "openstack_compute_floatingip_associate_v2" "myip" {
   floating_ip = "${openstack_networking_floatingip_v2.myip.address}"
   instance_id = "${openstack_compute_instance_v2.mi_tf_instance.id}"
 }
+
+########################################
+# Crear la Virtual Machine: node1
+########################################
+
+resource "openstack_compute_instance_v2" "mi_tf_node1" {
+  name      = "ub18node1-cnsa"
+  availability_zone = "nova"
+  image_id  = "9eabea01-c377-4911-9ee0-7276ae4ca820"    ## Ubuntu 18
+  flavor_name = "medium"
+  key_pair  = "${var.user_name}"  ## comprobar el nombre el Key Pair en OpenStack y cambiar si es necesario
+  security_groups = ["default"]
+  
+  network {
+    name = "${var.project_network}"
+  }
+
+}
+
+# Crear una IP de mi pool ext-net
+
+resource "openstack_networking_floatingip_v2" "myipnode1" {
+  pool = "ext-net"
+}
+
+# Asociar la IP
+resource "openstack_compute_floatingip_associate_v2" "myipnode1" {
+  floating_ip = "${openstack_networking_floatingip_v2.myipnode1.address}"
+  instance_id = "${openstack_compute_instance_v2.mi_tf_node1.id}"
+}
+
+########################################
+# Crear la Virtual Machine: node2
+########################################
+
+resource "openstack_compute_instance_v2" "mi_tf_node2" {
+  name      = "ub18node2-cnsa"
+  availability_zone = "nova"
+  image_id  = "9eabea01-c377-4911-9ee0-7276ae4ca820"    ## Ubuntu 18
+  flavor_name = "medium"
+  key_pair  = "${var.user_name}"
+  security_groups = ["default"]
+  
+  network {
+    name = "${var.project_network}"
+  }
+
+}
+
+# Crear una IP de mi pool ext-net
+
+resource "openstack_networking_floatingip_v2" "myipnode2" {
+  pool = "ext-net"
+}
+
+# Asociar la IP
+resource "openstack_compute_floatingip_associate_v2" "myipnode2" {
+  floating_ip = "${openstack_networking_floatingip_v2.myipnode2.address}"
+  instance_id = "${openstack_compute_instance_v2.mi_tf_node2.id}"
+}
+
 
